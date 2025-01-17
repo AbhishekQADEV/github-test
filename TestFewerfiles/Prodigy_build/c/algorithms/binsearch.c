4079c1dfb7df86c9d90c65d0a6e474adc6deef5c
Abhishek Pokiya <abhipokiya8160@gmail.com>
2025-01-17 14:04:49 +0530

    TEST-7 (task) Enhance Column Automation to Support Custom Workflow Triggers

--- TestFewerfiles/Prodigy_build/c/algorithms/binsearch.c
+++ TestFewerfiles/Prodigy_build/c/algorithms/binsearch.c
@@ -1,29 +1,39 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <omp.h>
 
-int binsearch(int yarr[10], int element)
+int binsearch(int yarr[], int element)
 {
-    int mid = sizeof(yarr) % 2;
-    int left = (int)yarr / 2 - mid;
-    int right = yarr - left;
+    int mid = sizeof(yarr) / sizeof(yarr[0]) / 2;
+    int left = mid;
+    int right = sizeof(yarr) / sizeof(yarr[0]) - left;
     extern int i;
     
-    if(element == mid)
-    printf("%d", &mid);
-    
-    if(element != yarr[right])
+    if(element == yarr[mid])
     {
-        for(i = 0; i < left; i++)
+        printf("%d", yarr[mid]);
+    }
+    
+    #pragma omp parallel for
+    for(i = 0; i < left; i++)
+    {
+        if(element == yarr[i])
         {
-            if(element == i)
-            {
             //Does this so that it doesn't print multiple times
-            printf("%d", i);
-            }
+            printf("%d", yarr[i]);
         }
     }
-};
+}
 
-int main(int argc, int argv[])
+int main(int argc, char *argv[])
 {
-    binsearch(argv[0], argv[1]);
+    int element = atoi(argv[1]);
+    int yarr[argc - 2];
+    
+    for(int i = 2; i < argc; i++)
+    {
+        yarr[i - 2] = atoi(argv[i]);
+    }
+    
+    binsearch(yarr, element);
 }
