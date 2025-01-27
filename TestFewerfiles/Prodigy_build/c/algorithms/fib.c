e8b682a67312598a9ad59162af18eef7ed52d8cb
Abhishek Pokiya <abhipokiya8160@gmail.com>
2025-01-27 12:02:52 +0530

    GITTH-175 (bug) 🚨 [SonarCloud] TestFewerfiles/Prodigy_build/c/algorithms/fib.c : 1 Issue(s) Detected

--- TestFewerfiles/Prodigy_build/c/algorithms/fib.c
+++ TestFewerfiles/Prodigy_build/c/algorithms/fib.c
@@ -11,11 +11,13 @@ int main()
     for(a = 0, b = 1; b > a; b++)
     {
         a++;
-        printf("%ld \n %ld", &a, &b);
+        printf("%ld \n %ld", a, b);
     }
     
     if(b == sizeof(long))
     {
         exit(1);
     }
+    
+    return 0;
 }
